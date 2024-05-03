c0f1a62a8adbdaa0532a21a637ebdd66c7f4c674
onflyer <salkaorama@gmail.com>
2024-05-03 22:14:50 +0200

    Initial Commit

---
+++ CodemagicHomeTask/CodemagicHomeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodemagicHomeTaskApp.swift
+//  CodemagicHomeTask
+//
+//  Created by Aleksandar Milidrag on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CodemagicHomeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodemagicHomeTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodemagicHomeTask
+//
+//  Created by Aleksandar Milidrag on 5/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
